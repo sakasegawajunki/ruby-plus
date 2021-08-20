cc0938adfac3359c502bb5e150cbac6961a93721
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-08-20 09:22:31 +0000

    4.3.14

---
+++ lib/4.3.rb
@@ -0,0 +1,20 @@
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum += n
+end
+puts sum
+
+a= [1,2,3,1,2,3,4]
+a.delete_if do |n|
+  n.odd?
+end
+p a
+
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even?? n*10:n
+  sum += sum_value
+end
+puts sum
