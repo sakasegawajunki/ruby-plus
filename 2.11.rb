8940cf347511876b67e803b88096b83f0695f0d6
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-08-20 00:32:10 +0000

    2.11

--- 2.11.rb
+++ 2.11.rb
@@ -15,3 +15,29 @@ puts foo
 
 puts "".empty?
 puts "abc".empty?
+
+puts"watch".include?("at")
+puts"watch".include?("in")
+
+puts 1.odd?
+puts 2.odd?
+
+puts 1.even?
+puts 2.even?
+
+puts nil.nil?
+puts "abc".nil?
+
+def multiple_of_three?(n)
+  n % 3 == 0
+end
+puts multiple_of_three?(1)
+puts multiple_of_three?(2)
+puts multiple_of_three?(3)
+
+a = "ruby"
+puts a.upcase
+puts a
+
+puts a.upcase!
+puts a
