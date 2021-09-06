2b16a7f727a4f9a29a896bcb63c41bb03615c7c9
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-06 00:48:55 +0000

    9.4

---
+++ lib/9.4.rb
@@ -0,0 +1,24 @@
+# users.each do |user|
+#   begin
+#     send_mail_to(user)
+#   rescue => e
+#     puts "#{e.class}: #{e.message}"
+#     puts e.backtrace
+#   end
+# end
+
+require "date"
+def convert_heisei_to_date(heisei_text)
+  m = heisei_text.match(/平成(?<jp_year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+  year = m[:jp_year].to_i + 1988
+  month = m[:month].to_i
+  day = m[:day].to_i
+
+  begin
+    Date.new(year, month, day)
+  rescue ArgumentError
+    nil
+  end
+end
+p convert_heisei_to_date("平成28年12月31日")
+p convert_heisei_to_date("平成28年99月99日")
